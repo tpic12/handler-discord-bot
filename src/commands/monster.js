e903af02f30935b311329efe1398a832a4aedf6b
Taylor Piccarreto <tpic12@gmail.com>
2020-07-20 17:19:03 -0400

    monster endpoint

---
+++ src/commands/monster.js
@@ -0,0 +1,38 @@
+const { execute } = require("./ping");
+
+module.exports = {
+  name: "monster",
+  description: "search for info on specific monster",
+  async execute(message, axios, api, name, embed, args) {
+    let getMonster = async () => {
+      let response = await axios.get(`${api}/monsters?q={"name":"${name}"}`);
+      let monster = response.data;
+      return monster;
+    };
+    let monsterValue = await getMonster();
+    if (!monsterValue.response) {
+      return message.reply(
+        `Sorry, I can't find that monster in my research notes!`
+      );
+    }
+
+    let weakness3 = [];
+    let weakness2 = [];
+    for (value in monsterValue[0].weaknesses) {
+      if (monsterValue[0].weaknesses[value].stars === 3) {
+        weakness3.push(monsterValue[0].weaknesses[value].element);
+      } else if (monsterValue[0].weaknesses[value].stars === 2) {
+        weakness2.push(monsterValue[0].weaknesses[value].element);
+      }
+    }
+
+    embed
+      .setTitle(monsterValue[0].name)
+      .addField("Species", monsterValue[0].species)
+      .addField("3 Star Weaknesses", weakness3.join(", "))
+      .addField("2 Star Weaknesses", weakness2.join(", "))
+      .addField("Resistances", monsterValue[0].resistances[0].element)
+      .setColor(0x48c9b0);
+    message.channel.send(embed);
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const axios = require("axios");
 const bot = new Discord.Client();
-const { TOKEN } = require("../config");
+const { TOKEN, API_ENDPOINT } = require("../config");
 const fs = require("fs");
 const PREFIX = "*";
 const version = "1.0.0";
@@ -24,6 +24,8 @@ bot.on("ready", () => {
 
 bot.on("message", async (message) => {
   let args = message.content.substring(PREFIX.length).split(" ");
+  let api = API_ENDPOINT;
+  let embed = new Discord.MessageEmbed();
 
   switch (args[0]) {
     case "ping":
@@ -32,19 +34,17 @@ bot.on("message", async (message) => {
     case "site":
       message.channel.send("http://www.taylorpiccarreto.com");
       break;
-    case "info":
-      if (args[1] === "version") {
-        message.channel.send(`Version ${version}`);
-      } else {
-        message.channel.send(`${args[1]} is not a valid command`);
-      }
-      break;
     case "embed":
-      const embed = new Discord.MessageEmbed();
       bot.commands.get("embed").execute(message, embed);
       break;
     case "joke":
       bot.commands.get("joke").execute(message, axios, args);
       break;
+    case "monster":
+      let name = args.slice(1).join(" ");
+      if (!args[1]) return message.reply("Please specify monster to search");
+      bot.commands
+        .get("monster")
+        .execute(message, axios, api, name, embed, args);
   }
 });
