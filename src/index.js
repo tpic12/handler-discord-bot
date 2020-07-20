fca4cc94c2e3ff2508b4c4d7aff8c8de334d55ae
Taylor Piccarreto <tpic12@gmail.com>
2020-07-20 15:00:58 -0400

    add embeds and commands dir

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const { TOKEN } = require("../config");
+const fs = require("fs");
+const PREFIX = "*";
+const version = "1.0.0";
+
+bot.commands = new Discord.Collection();
+const commandFiles = fs
+  .readdirSync("./src/commands/")
+  .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  bot.commands.set(command.name, command);
+}
 
 bot.login(TOKEN);
 
@@ -8,8 +21,32 @@ bot.on("ready", () => {
   console.log("Handler is Online!");
 });
 
-bot.on("message", (msg) => {
-  if (msg.content[0] === "*") {
-    msg.reply("Hello pal!");
+bot.on("message", (message) => {
+  let args = message.content.substring(PREFIX.length).split(" ");
+
+  switch (args[0]) {
+    case "ping":
+      bot.commands.get("ping").execute(message, args);
+      break;
+    case "site":
+      message.channel.send("http://www.taylorpiccarreto.com");
+      break;
+    case "info":
+      if (args[1] === "version") {
+        message.channel.send(`Version ${version}`);
+      } else {
+        message.channel.send(`${args[1]} is not a valid command`);
+      }
+      break;
+    case "embed":
+      const embed = new Discord.MessageEmbed()
+        .setTitle("User Info")
+        .addField("Player Name", message.author.username)
+        .addField("Version", version)
+        .addField("Current Server", message.guild.name)
+        .setThumbnail(message.author.avatarURL())
+        .setColor(0x48c9b0);
+      message.channel.send(embed);
+      break;
   }
 });
